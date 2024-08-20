1633591f7dc752d48e8cbc84b6a49020a7878c93
nakashimashin <nakashimashin0517@gmail.com>
2024-08-21 00:08:12 +0900

    vuetifyを使えるようにする

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,22 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+// main.ts
+import { createApp } from 'vue';
+import App from './App.vue';
+import { createVuetify } from 'vuetify';
+import 'vuetify/styles';
+import { aliases, mdi } from 'vuetify/iconsets/mdi';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
 
-createApp(App).mount('#app')
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
+});
+
+createApp(App).use(vuetify).mount('#app');
